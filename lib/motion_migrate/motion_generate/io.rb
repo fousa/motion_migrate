d44a560d836bbf15919858862db8247168b7b80a
Jelle Vandebeeck <jelle@fousa.be>
2013-01-29 17:56:23 +0100

    Write the xml to the project

---
+++ lib/motion_migrate/motion_generate/io.rb
@@ -0,0 +1,80 @@
+module MotionMigrate
+  class IO
+    class << self
+      def write(xml="")
+        create_db
+
+        current_schema = nil
+        if version = current_schema_version()
+          File.open(File.join(current_schema(), "contents")) do |f|
+            current_schema = f.read
+          end
+        end
+
+        if current_schema != xml
+          version = (version || 0) + 1
+
+          latest_schema_path = xcdatamodeld_path("schema.#{version}.xcdatamodel")
+          Dir.mkdir(latest_schema_path) unless Dir.exists?(latest_schema_path)
+
+          File.open(File.join(latest_schema_path, "contents"), "w") do |file|
+            file.write(xml)
+          end
+          write_current_schema(version)
+
+          unless File.symlink?("resources/schema.xcdatamodeld")
+            File.symlink("../db/schema.xcdatamodeld", "resources/schema.xcdatamodeld")
+          end
+
+          length = 40 + version.to_s.length
+          puts "#{"-" * length}\n--- Data model migrated to version #{version} ---\n#{"-" * length}"
+        else
+          length = 39 + version.to_s.length
+          puts "#{"-" * length}\n--- Data model already at version #{version} ---\n#{"-" * length}"
+        end
+      end
+
+      protected
+
+      def create_db
+        FileUtils.mkdir_p(File.join("db", "schema.xcdatamodeld"))
+      end
+
+      def xcdatamodeld_path(*args)
+        File.join(["db", "schema.xcdatamodeld"] + args)
+      end
+
+      def current_schema
+        plist = xcdatamodeld_path(".xccurrentversion")
+        return nil unless File.exists?(plist)
+
+        xcdatamodeld_path(Nokogiri::XML(File.open(plist)).at_xpath("/plist/dict/string").text)
+      end
+
+      def current_schema_version
+        return nil unless path = current_schema
+
+        version = nil
+        path.match(/\.([0-9]+)\.xcdatamodel$/) do |match|
+          version = match[1].to_i
+        end
+        version
+      end
+      
+      def write_current_schema(version)
+        File.open(xcdatamodeld_path(".xccurrentversion"), "w") do |file|
+          file.write(<<-PLIST)
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>_XCCurrentVersionName</key>
+  <string>schema.#{version}.xcdatamodel</string>
+</dict>
+</plist>
+          PLIST
+        end
+      end
+    end
+  end
+end
--- lib/tasks/migrate.rake
+++ lib/tasks/migrate.rake
@@ -3,11 +3,13 @@ require "nokogiri"
 require "motion_migrate/motion_generate/entity"
 require "motion_migrate/motion_generate/property"
 require "motion_migrate/generate"
+require "motion_migrate/motion_generate/io"
 
 namespace :db do
   desc "Generate a version of the current database model as described in the models."
   task :migrate do
-    puts MotionMigrate::Generate.build
+    schema_xml = MotionMigrate::Generate.build
+    MotionMigrate::IO.write(schema_xml)
   end
 
   desc "Go back to the previous version of the database model."
