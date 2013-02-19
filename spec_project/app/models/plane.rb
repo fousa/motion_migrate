60d91f74cff13eb021f602eb56d2cb45eff117f2
Jelle Vandebeeck <jelle@fousa.be>
2013-02-19 15:02:55 +0100

    Add date test to flow

--- spec_project/app/models/plane.rb
+++ spec_project/app/models/plane.rb
@@ -1,4 +1,5 @@
 class Plane < MotionMigrate::Model
   property :name, :string
   property :multi_engine, :boolean, :default => false
+  property :first_flight_at, :date
 end
--- spec_project/spec/property_spec.rb
+++ spec_project/spec/property_spec.rb
@@ -1,29 +1,52 @@
 describe "properties in a model" do
   it "should set the property name on the model" do
-    Plane.MR_truncateAll
+    reset
 
     new_plane = Plane.MR_createEntity
     new_plane.name.should == nil
     new_plane.name = "Airbus 380"
     new_plane.name.should == "Airbus 380"
-    NSManagedObjectContext.contextForCurrentThread.MR_saveToPersistentStoreAndWait
+    save
 
     fetched_plane = Plane.MR_findFirst
     fetched_plane.name.should == "Airbus 380"
   end
 
   it "should set the boolean property on the model" do
-    Plane.MR_truncateAll
+    reset
     
     new_plane = Plane.MR_createEntity
     new_plane.multi_engine.boolValue.should == false
     new_plane.multi_engine = true
-    new_plane.multi_engine.boolValue.should == true
-    NSManagedObjectContext.contextForCurrentThread.MR_saveToPersistentStoreAndWait
+    new_plane.multi_engine.class.should == Fixnum
+    save
 
     fetched_plane = Plane.MR_findFirst
     fetched_plane.multi_engine.boolValue.should == true
   end
 
-  # Check date
+  it "should set the date property on the model" do
+    reset
+    
+    new_plane = Plane.MR_createEntity
+    new_plane.first_flight_at.should == nil
+    date = NSDate.date
+    new_plane.first_flight_at = date
+    new_plane.first_flight_at.should == date
+    new_plane.first_flight_at.class.should == Time
+    save
+
+    fetched_plane = Plane.MR_findFirst
+    fetched_plane.first_flight_at.should == date
+  end
+
+  # Check data
+
+  def reset
+    Plane.MR_truncateAll
+  end
+
+  def save
+    NSManagedObjectContext.contextForCurrentThread.MR_saveToPersistentStoreAndWait
+  end
 end
